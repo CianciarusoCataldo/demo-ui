e73478185da5d165c1df0fb9484d8b77ecac526b
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2021-04-12 01:31:46 +0200

    updates

--- src/components/Previewer/index.compositions.tsx
+++ src/components/Previewer/index.compositions.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import Previewer from "./";
 
 export const BasicExample = () => (
-  <Previewer label="This is a Previewer">
-    <div style={{ margin: "2rem", padding: "2rem" }}>Children</div>
+  <Previewer label="This is a Previewer" screenshot>
+    <div style={{ margin: "2rem", padding: "2rem" }}>
+      <p>Children</p>
+    </div>
   </Previewer>
 );
