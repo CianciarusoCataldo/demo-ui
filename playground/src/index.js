bb01078f6e28539a12c0473fc1aaac166b358df1
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2021-04-15 03:36:37 +0200

    setup demo-ui github page

--- playground/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./index.css";
-
-import Playground from "./Playground";
-
-ReactDOM.render(<Playground />, document.getElementById("root"));
